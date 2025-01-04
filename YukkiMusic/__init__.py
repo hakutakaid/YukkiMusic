055f5f16ab349ebf0953115f594b37d90963153e
hakutakaid <rachmad.alfio@gmail.com>
2025-01-05 01:26:50 +0700

    Yukki Music

---
+++ YukkiMusic/__init__.py
@@ -0,0 +1,44 @@
+#
+
+from YukkiMusic.core.bot import YukkiBot
+from YukkiMusic.core.dir import dirr
+from YukkiMusic.core.git import git
+from YukkiMusic.core.cookies import save_cookies
+from YukkiMusic.core.userbot import Userbot
+from YukkiMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = YukkiBot()
+
+# Cookies
+save_cookies()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
